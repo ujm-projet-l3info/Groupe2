6f86260cc1a7caa304155d5b442cbefd988a0bb9
kabrial <kevin.abrial@hotmail.fr>
2016-03-01 16:02:14 +0100

    Version 2 de la base de donnee sql

--- application_android_projet.sql
+++ application_android_projet.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Mar 01 Mars 2016 à 15:29
+-- Généré le :  Mar 01 Mars 2016 à 15:58
 -- Version du serveur :  5.6.28-0ubuntu0.15.10.1
 -- Version de PHP :  5.6.11-1ubuntu3.1
 
@@ -23,6 +23,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `Cle`
+--
+
+CREATE TABLE IF NOT EXISTS `Cle` (
+  `id_cle` int(11) NOT NULL,
+  `libelle` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `Information`
 --
 
@@ -30,10 +41,46 @@ CREATE TABLE IF NOT EXISTS `Information` (
   `id` int(11) NOT NULL,
   `latitude` float NOT NULL,
   `longitude` float NOT NULL,
-  `cle` varchar(32) NOT NULL,
+  `cle` int(11) NOT NULL,
   `valeur` varchar(32) NOT NULL,
   `lien_url` varchar(128) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `Cle`
+--
+ALTER TABLE `Cle`
+  ADD PRIMARY KEY (`id_cle`);
+
+--
+-- Index pour la table `Information`
+--
+ALTER TABLE `Information`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `Information` (`cle`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Cle`
+--
+ALTER TABLE `Cle`
+  MODIFY `id_cle` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Information`
+--
+ALTER TABLE `Information`
+  ADD CONSTRAINT `Information_ibfk_1` FOREIGN KEY (`cle`) REFERENCES `Cle` (`id_cle`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
