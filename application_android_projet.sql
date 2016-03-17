2d45505cc6fa638d4dcf8bc8dff760d7fd6ffe3b
Fl0rc <florent.chastagner@gmail.com>
2016-03-17 17:03:47 +0100

    Mise à jour de la base de donnée (meilleure gestion des tags)
    Signed-off-by: Fl0rc <florent.chastagner@gmail.com>

--- application_android_projet.sql
+++ application_android_projet.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.13.1deb1
+-- version 4.1.4
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Mar 01 Mars 2016 à 17:23
--- Version du serveur :  5.6.28-0ubuntu0.15.10.1
--- Version de PHP :  5.6.11-1ubuntu3.1
+-- Client :  127.0.0.1
+-- Généré le :  Jeu 17 Mars 2016 à 16:56
+-- Version du serveur :  5.6.15-log
+-- Version de PHP :  5.5.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Base de données :  `application_android_projet`
@@ -23,64 +23,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Cle`
+-- Structure de la table `cle`
 --
 
-CREATE TABLE IF NOT EXISTS `Cle` (
-  `id_cle` int(11) NOT NULL,
-  `libelle` varchar(32) NOT NULL
+CREATE TABLE IF NOT EXISTS `cle` (
+  `id_cle` int(11) NOT NULL AUTO_INCREMENT,
+  `libelle` varchar(32) NOT NULL,
+  PRIMARY KEY (`id_cle`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=26 ;
+
+--
+-- Contenu de la table `cle`
+--
+
+INSERT INTO `cle` (`id_cle`, `libelle`) VALUES
+(1, 'amenity'),
+(2, 'building'),
+(3, 'historic'),
+(4, 'leisure'),
+(5, 'nature'),
+(6, 'shop'),
+(7, 'sport'),
+(8, 'tourism'),
+(9, 'waterway'),
+(10, 'name'),
+(11, 'addr:street'),
+(12, 'addr:housenumber'),
+(13, 'addr:city'),
+(14, 'addr:postcode'),
+(15, 'opening_hours'),
+(16, 'website'),
+(17, 'addr:country'),
+(18, 'information'),
+(19, 'wikipedia'),
+(20, 'image'),
+(21, 'cuisine'),
+(22, 'religion'),
+(23, 'source'),
+(24, 'building:levels'),
+(25, 'height');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `information`
+--
+
+CREATE TABLE IF NOT EXISTS `information` (
+  `id` int(11) NOT NULL,
+  `latitude` float NOT NULL,
+  `longitude` float NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `Information`
+-- Structure de la table `tag`
 --
 
-CREATE TABLE IF NOT EXISTS `Information` (
-  `id` int(11) NOT NULL,
-  `latitude` float NOT NULL,
-  `longitude` float NOT NULL,
-  `cle` int(11) NOT NULL,
-  `valeur` varchar(32) NOT NULL,
-  `xml` text
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `Cle`
---
-ALTER TABLE `Cle`
-  ADD PRIMARY KEY (`id_cle`);
-
---
--- Index pour la table `Information`
---
-ALTER TABLE `Information`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `Information` (`cle`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `Cle`
---
-ALTER TABLE `Cle`
-  MODIFY `id_cle` int(11) NOT NULL AUTO_INCREMENT;
---
--- Contraintes pour les tables exportées
---
-
---
--- Contraintes pour la table `Information`
---
-ALTER TABLE `Information`
-  ADD CONSTRAINT `Information_ibfk_1` FOREIGN KEY (`cle`) REFERENCES `Cle` (`id_cle`);
+CREATE TABLE IF NOT EXISTS `tag` (
+  `id_info` int(11) NOT NULL,
+  `id_cle` int(11) NOT NULL,
+  `valeur` varchar(100) NOT NULL,
+  PRIMARY KEY (`id_info`,`id_cle`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
